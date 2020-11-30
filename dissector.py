a58da995a2108415d16a7ea3587d51a30b131ccb
rwiddhi chakraborty <dejavu.chakraborty@gmail.com>
2020-11-30 17:24:21 +0100

    Dissector v2.1 (#10)
    * Minor fixes
    
    * Functional API for building sub-models
    
    * Fixed minor issues

--- dissector.py
+++ dissector.py
@@ -24,15 +24,16 @@
 
         Output: Array of SV scores of length k, hopefully ordered by shallow to deep sub models.
 
-    5. Function to calculate PV score for all x_test
-        Input: a) x_test, array_sv_score, weights_array
-        Output: Array of PV scores in (0,1).
 
-    6. Function to calculate weights array [3 options: Logarithmic, Linear, Exponential]:
+    5. Function to calculate weights array [3 options: Logarithmic, Linear, Exponential]:
         Input: a) Type of weight growth
                b) Number k of specific layer
 
         Output: array of weights, shape: (#sub_models,)
+
+    6. Function to calculate PV score for all x_test
+        Input: a) x_test, array_sv_score, weights_array
+        Output: Array of PV scores in (0,1).
 
     7. Function calculate AUC score:
         Inputs: a) generate_ground_truth(model, test_pred, x_test)
@@ -48,20 +49,25 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import ZeroPadding2D, Dense, Flatten
 import os
 from tqdm import tqdm
+import time
 
 class Dissector():
 
-    def __init__(self, model: tf.keras.Model, model_path: str, sub_model_path: str):
+    def __init__(self, model: tf.keras.Model, model_path: str, sub_model_path: str, num_classes: int, opt = {}):
 
         self.model = model
         self.model_path = model_path
         self.sub_model_path = sub_model_path
+        self.num_classes = num_classes
+        self.opt = opt
 
 
     def generate_ground_truth(self, x_test: tf.data.Dataset, y_test: tf.data.Dataset):
 
+        #g_truths = np.ones(shape=x_test.shape[0], dtype = bool)
         #model = load_model(self.args['model_path'])
-        test_preds = self.model.predict_classes(x_test)
+        test_preds = self.model.predict(x_test)
+        test_preds = np.argmax(test_preds, axis=1)
         labels = np.argmax(y_test, axis=1)
         corr = np.where(test_preds == labels)
         incorr = np.where(test_preds != labels)
@@ -72,6 +78,20 @@ class Dissector():
 
     def generate_sub_models(self, layer_list: []):
 
+        """Two options below: Either a sequential (for list of ints) or a functional (list of strs)"""
+        #TODO Config for save path and model type
+
+        # for i, l_name in enumerate(l_names):
+        #     mid = self.model.get_layer(l_name).output
+        #     flat = Flatten()(mid)
+        #     final = Dense(10, activation='softmax')(flat)
+        #     n_model = Model(self.model.input, final)
+        #     for l in n_model.layers[:-1]:
+        #         l.trainable = False
+        #
+        #     n_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        #     n_model.save("./submodels_dissector/submodel_{}_lenet4_{}.h5".format(i, 'mnist'))
+
         for _, l in enumerate(layer_list):
             n_model = Sequential()
             for layer in self.model.layers[0:l]:
@@ -81,8 +101,8 @@ class Dissector():
             for layer in n_model.layers:
                 layer.trainable = False
 
-            n_model.add(Dense(10, activation='softmax'))
-            n_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+            n_model.add(Dense(self.num_classes, activation='softmax'))
+            n_model.compile(loss=self.opt['loss'], optimizer=self.opt['optimizer'], metrics=self.opt['metrics'])
             n_model.save(self.sub_model_path+"submodel_{}_lenet4_{}.h5".format(l, 'mnist'))
 
     def train_sub_models(self, x_train: tf.data.Dataset, y_train: tf.data.Dataset, epochs: int):
@@ -99,25 +119,27 @@ class Dissector():
                 batch_size=128,
                 shuffle=True,
                 verbose=0,
-                validation_split=0.2,
+                validation_split=self.opt['val_split'],
             )
 
             s_model.save(self.sub_model_path+m)
 
 
     def sv_score(self, x_test: tf.data.Dataset):
+        """Check out 'Prediction confidence score (PCS)' - michael will give you reference for msc thesis"""
 
         m = load_model(self.model_path)
-        test_preds = m.predict_classes(x_test)
+        test_preds = np.argmax(m.predict(x_test), axis=1)
         sub_models = os.listdir(self.sub_model_path)
-        scores = {}
+        print(sub_models)
+        scores = np.empty(shape= (len(sub_models), (x_test.shape[0])))
 
-        for i, m in enumerate(sub_models):
+        for index, m in enumerate(sub_models):
             sv_scores = []
             s_model = load_model(self.sub_model_path + m)
 
             activations = s_model.predict(x_test)
-            preds = s_model.predict_classes(x_test)
+            preds = np.argmax(activations, axis=1)
 
             for i, p in enumerate(preds):
 
@@ -136,17 +158,18 @@ class Dissector():
 
                 sv_scores.append(sv_score)
 
-            scores[m] = sv_scores
+            scores[index] = np.array(sv_scores)
 
         return scores
 
     def get_weights(self, growth_type: str, alpha: float):
 
         sub_models = os.listdir(self.sub_model_path)
-        weights = {}
+        print(sub_models)
+        weights = np.empty(shape=(len(sub_models), ))
 
         assert growth_type in ['linear', 'logarithmic', 'exponential'], "Invalid weight growth type"
-        for m in sub_models:
+        for i, m in enumerate(sub_models):
             l_number = int(m.split("_")[1]) + 1
 
             if growth_type == 'linear':
@@ -161,18 +184,13 @@ class Dissector():
 
                 y = np.exp(alpha * l_number)
 
-            weights[m] = y
+            weights[i] = y
 
         return weights
 
-    def pv_scores(self, weights: {}, scores: {}):
+    def pv_scores(self, weights: np.ndarray, scores: np.ndarray):
 
-        pv_scores = np.zeros(shape=(10000,))
-        for sub_model, sv in scores.items():
-            weighted_score = weights[sub_model] * np.array(sv)
-            pv_scores += weighted_score
-
-        pv_scores = pv_scores / sum(weights.values())
+        pv_scores = np.sum(np.multiply(scores, weights[:, np.newaxis]), axis=0) / np.sum(weights)
 
         return pv_scores
 
