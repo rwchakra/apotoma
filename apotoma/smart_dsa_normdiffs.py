8351f0039f755ef912c61049b193d7ec178748df
Michael Weiss <code@mweiss.ch>
2021-01-04 12:21:33 +0100

    Smart DSAs (#35)
    * Diff of norms and Norm of diffs
    
    * Minor changes to smart dsa
    
    * Added default threshold values
    
    Co-authored-by: rwiddhi chakraborty <dejavu.chakraborty@gmail.com>

---
+++ apotoma/smart_dsa_normdiffs.py
@@ -0,0 +1,90 @@
+import os
+from typing import Tuple
+
+import numpy as np
+import tensorflow as tf
+
+from apotoma.surprise_adequacy import DSA, SurpriseAdequacyConfig
+
+
+class SmartDSA(DSA):
+
+    def __init__(self,
+                 model: tf.keras.Model,
+                 train_data: np.ndarray,
+                 config: SurpriseAdequacyConfig,
+                 threshold=0.05,
+                 dsa_batch_size=500) -> None:
+        super().__init__(model, train_data, config, dsa_batch_size)
+        self.threshold = threshold
+
+    def _load_or_calc_train_ats(self, use_cache=False) -> None:
+
+        saved_train_path = self._get_saved_path("train")
+
+        if os.path.exists(saved_train_path[0]) and use_cache:
+            print("Found saved {} ATs, skip serving".format("train"))
+            # In case train_ats is stored in a disk
+            self.train_ats, self.train_pred = np.load(saved_train_path[0]), np.load(saved_train_path[1])
+
+        else:
+            self.train_ats, self.train_pred = self._load_or_calculate_ats(dataset=self.train_data, ds_type="train",
+                                                                          use_cache=use_cache)
+
+        self._select_smart_ats()
+
+    def prep(self, use_cache: bool = False) -> None:
+        self._load_or_calc_train_ats(use_cache=use_cache)
+
+    def _select_smart_ats(self):
+
+        all_train_ats = self.train_ats
+        all_train_pred = self.train_pred
+
+        new_class_matrix_norms_vec = {}
+        for label in range(10):
+
+            data_label = all_train_ats[np.where(all_train_pred == label)]
+            available_indices = np.where(all_train_pred == label)[0]
+
+
+            indexes = np.arange(available_indices.shape[0])
+            is_available = np.ones(shape=available_indices.shape[0], dtype=bool)
+
+            # This index used in the loop indicates the latest element selected to be added to chosen items
+            current_idx = 0
+
+            # for logging only
+            num_steps = 0
+
+            while True:
+                num_steps += 1
+                # Get all indexes (higher than current_index) which are still available and the corresponding ats
+                candidate_indexes = np.argwhere((indexes > current_idx) & is_available).flatten()
+                candidates = data_label[candidate_indexes]
+                print(f"candidates shape = {candidates.shape}")
+
+
+                diffs = np.linalg.norm(candidates - data_label[current_idx], axis=1)
+                assert diffs.ndim == 1
+
+                # Identify candidates which are too similar to currently added element (current_idx)
+                # and set their availability to false
+                remove_candidate_indexes = np.flatnonzero(diffs < self.threshold)
+                remove_overall_indexes = candidate_indexes[remove_candidate_indexes]
+                is_available[remove_overall_indexes] = False
+
+                # Select the next available candidate as current_idx (i.e., use select it for use in dsa),
+                #   or break if none available
+                if np.count_nonzero(is_available[current_idx:]) > 1:
+                    print(len(is_available[current_idx:]))
+                    current_idx = np.argmax(is_available[current_idx + 1:]) + (current_idx + 1)
+                else:
+                    # np.argmax did not find anything
+                    break
+
+            selected_indexes = np.nonzero(is_available)[0]
+            new_class_matrix_norms_vec[label] = list(available_indices[selected_indexes])
+
+        self.number_of_samples = sum(len(lst) for lst in new_class_matrix_norms_vec.values())
+        self.class_matrix = new_class_matrix_norms_vec
--- apotoma/xxx_example_will_be_deleted.py
+++ apotoma/xxx_example_will_be_deleted.py
@@ -2,7 +2,8 @@ from random import random
 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.datasets import mnist
-from apotoma.surprise_adequacy import DSA, LSA
+from apotoma.surprise_adequacy import DSA, LSA, SurpriseAdequacyConfig
+from apotoma.smart_dsa_normdiffs import SmartDSA
 from evaluate import _auc_roc
 from train_model import CLIP_MAX, CLIP_MIN
 
@@ -29,6 +30,9 @@ if __name__ == '__main__':
     assert args['lsa'] ^ args['dsa'], "Select either 'lsa' or 'dsa'"
     print(args)
 
+    config = SurpriseAdequacyConfig(is_classification=True, ds_name='mnist', layer_names=['activation_3']
+                                    , saved_path='/Users/rwiddhichakraborty/PycharmProjects/Thesis/apotoma/tmp1', num_classes=10)
+
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = x_train.reshape(-1, 28, 28, 1)
     x_test = x_test.reshape(-1, 28, 28, 1)
@@ -39,26 +43,27 @@ if __name__ == '__main__':
     x_test = (x_test / 255.0) - (1.0 - CLIP_MAX)
 
     # Load pre-trained model.
-    model = load_model("./model/model_mnist.h5")
+    model = load_model("/Users/rwiddhichakraborty/PycharmProjects/Thesis/apotoma/model/model_mnist.h5")
     model.summary()
 
 
     # Load target set. Fixed for now, will change later, added as an args element
     #x_target = np.load("./adv/adv_mnist_{}.npy".format(args.target))
-    x_target = np.load("./adv/adv_mnist_fgsm.npy")
+    x_target = np.load("/Users/rwiddhichakraborty/PycharmProjects/Thesis/apotoma/adv/adv_mnist_fgsm.npy")
 
     # Usage of Library
-    novelty_score = None
-    if args['dsa']:
+    '''if args['dsa']:
         novelty_score = DSA(train_data=x_train, model=model, args=args)
     else:
-        novelty_score = LSA(train_data=x_train, model=model, args=args)
+        novelty_score = LSA(train_data=x_train, model=model, args=args)'''
+
+    novelty_score = SmartDSA(model=model, config=config, train_data=x_train, number_of_samples=100)
 
 
 
     novelty_score.prep()
-    sa_test = novelty_score.calc(x_test, "test", use_cache=False)
-    sa_target = novelty_score.calc(x_target, "target", use_cache=False)
+    sa_test = novelty_score.calc(x_test, "test", use_cache=False)[0]
+    sa_target = novelty_score.calc(x_target, "target", use_cache=False)[0]
 
     # Evaluation
     score = _auc_roc(sa_test, sa_target)
