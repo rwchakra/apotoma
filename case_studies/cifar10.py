aff0acd8181d01c1184a1857a4544501bb89d83c
Michael Weiss <code@mweiss.ch>
2021-01-06 08:36:07 +0100

    Start setting up experiment runner (#34)

---
+++ case_studies/cifar10.py
@@ -0,0 +1,82 @@
+from typing import Dict
+
+import tensorflow as tf
+import uncertainty_wizard as uwiz
+
+from case_studies import config
+
+NUM_MODELS = 200
+
+
+class TrainContext(uwiz.models.ensemble_utils.DeviceAllocatorContextManager):
+
+    @classmethod
+    def file_path(cls) -> str:
+        return "temp-ensemble.txt"
+
+
+    @classmethod
+    def run_on_cpu(cls) -> bool:
+        return False
+
+    @classmethod
+    def virtual_devices_per_gpu(cls) -> Dict[int, int]:
+        return {
+            0: 3,
+            1: 3
+        }
+
+    @classmethod
+    def gpu_memory_limit(cls) -> int:
+        return 1500
+
+
+def train_model(model_id):
+    import tensorflow as tf
+
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same',
+                                     input_shape=(32, 32, 3)))
+    model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(128, activation='relu', name="2nd_last_dense"))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Dense(32, activation='relu', name="last_dense"))
+    model.add(tf.keras.layers.Dense(10, activation='softmax', name="sm_output"))
+
+    opt = tf.keras.optimizers.SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    (x_train, y_train), (_, _) = tf.keras.datasets.cifar10.load_data()
+    x_train = x_train / 255.
+    y_train = tf.keras.utils.to_categorical(y_train, 10)
+
+    # For the sake of this example, let's use just one epoch.
+    # Of course, for higher accuracy, you should use more.
+    model.fit(x_train, y_train, batch_size=32, epochs=100, validation_split=0.1,
+              callbacks=[tf.keras.callbacks.EarlyStopping(patience=2)])
+
+
+    return model, "history_not_returned"
+
+
+if __name__ == '__main__':
+    # Prepare dataset in cache
+    tf.keras.datasets.cifar10.load_data()
+
+    model_collection = uwiz.models.LazyEnsemble(num_models=NUM_MODELS,
+                                                model_save_path=config.MODELS_BASE_FOLDER + "cifar10",
+                                                delete_existing=False)
+    histories = model_collection.create(
+        train_model, num_processes=6, context=TrainContext
+    )
---
+++ case_studies/utils.py
@@ -0,0 +1,139 @@
+import os
+import pickle
+import time
+from typing import Dict, Tuple, List
+
+import numpy as np
+from dataclasses import dataclass
+from sklearn import metrics
+
+from apotoma.smart_dsa_diffnorms import DiffOfNormsSelectiveDSA
+from apotoma.smart_dsa_normdiffs import NormOfDiffsSelectiveDSA
+from apotoma.surprise_adequacy import LSA, DSA, SurpriseAdequacyConfig, SurpriseAdequacy
+from case_studies import config
+
+# We accept redundant at calculation for now - may change this later
+USE_CACHE = False
+
+
+class Result:
+    def __init__(self,
+                 name: str,
+                 prepare_time: float,
+                 approach_custom_info: Dict):
+        self.name = name
+        self.prepare_time = prepare_time
+        self.approach_custom_info = approach_custom_info
+        self.evals: Dict[str, 'TestSetEval'] = dict()
+
+
+@dataclass
+class TestSetEval:
+    eval_time: float
+    # avg_pr_score: float
+    ood_auc_roc: float
+    num_nominal_samples: int
+    num_outlier_samples: int
+    # TODO maybe we can add a tpr at fpr.05 or something as well
+
+
+def run_experiments(model,
+                    sa_config: SurpriseAdequacyConfig,
+                    train_x: np.ndarray,
+                    test_data: Dict[str, Tuple[np.ndarray, np.ndarray]]) -> List[Result]:
+    results = []
+
+    nominal_data = test_data.pop("nominal")
+
+    for train_percent in range(5, 101, 5):
+        num_samples = int(train_x.shape[0] * train_percent / 100)
+        train_subset = train_x[:num_samples]
+        # DSA
+        dsa = DSA(model=model, train_data=train_subset, config=sa_config, dsa_batch_size=config.DSA_BATCH_SIZE)
+        dsa_custom_info = {"sum_samples": num_samples, "dsa_batch_size": config.DSA_BATCH_SIZE}
+        results.append(eval_for_sa(f"dsa_rand{train_percent}_perc", dsa, dsa_custom_info, nominal_data, test_data, ))
+        # LSA
+        lsa = LSA(model=model, train_data=train_subset, config=sa_config)
+        lsa_custom_info = {"num_samples": num_samples}
+        results.append(eval_for_sa(f"lsa_rand{train_percent}_perc", lsa, lsa_custom_info, nominal_data, test_data))
+
+    # TODO We need to come up with dynamic way to calculate these
+    for diff_threshold in [0.1, 0.001, 0.0001, .00001, 0.000001]:
+        dsa = DiffOfNormsSelectiveDSA(model=model,
+                                      train_data=train_x,
+                                      config=sa_config,
+                                      dsa_batch_size=config.DSA_BATCH_SIZE,
+                                      threshold=diff_threshold)
+        dsa_custom_info = {
+            "diff_threshold": diff_threshold,
+            "dsa_batch_size": config.DSA_BATCH_SIZE
+        }
+        results.append(eval_for_sa(f"dsa_don_{diff_threshold}", dsa, dsa_custom_info, nominal_data, test_data))
+
+    # TODO We need to come up with dynamic way to calculate these
+    for diff_threshold in range(10, 90, 10):
+        diff_threshold /= 100  # int percentage to float ratio
+        dsa = NormOfDiffsSelectiveDSA(model=model,
+                                      train_data=train_x,
+                                      config=sa_config,
+                                      dsa_batch_size=config.DSA_BATCH_SIZE,
+                                      threshold=diff_threshold)
+        dsa_custom_info = {
+            "diff_threshold": diff_threshold,
+            "dsa_batch_size": config.DSA_BATCH_SIZE
+        }
+        results.append(eval_for_sa(f"dsa_nod_{diff_threshold}", dsa, dsa_custom_info, nominal_data, test_data))
+
+    return results
+
+
+def eval_for_sa(sa_name,
+                sa: SurpriseAdequacy,
+                approach_custom_info: Dict,
+                nominal_data: Tuple[np.ndarray, np.ndarray],
+                test_data: Dict[str, Tuple[np.ndarray, np.ndarray]]) -> Result:
+    # Prepare SA (offline)
+    prep_start_time = time.time()
+    # TODO split DNN prediction and SA postprocessing (e.g. kde fitting)
+    sa.prep(use_cache=USE_CACHE)
+    prep_time = time.time() - prep_start_time
+    if isinstance(sa, DiffOfNormsSelectiveDSA) or isinstance(sa, NormOfDiffsSelectiveDSA):
+        approach_custom_info['num_samples'] = sa.number_of_samples
+
+    # Create result object
+    result = Result(name=sa_name, prepare_time=prep_time, approach_custom_info=approach_custom_info)
+
+    nom_surp, nom_pred = sa.calc(target_data=nominal_data[0], use_cache=USE_CACHE, ds_type='test')
+
+    for test_set_name, test_set in test_data.items():
+        print(f"Evaluating {sa_name} with test set {test_set_name}")
+        x_test = test_set[0]
+
+        calc_start = time.time()
+        # TODO split DNN prediction and SA calculation
+        surp, pred = sa.calc(target_data=x_test, use_cache=USE_CACHE, ds_type='test')
+        calc_time = time.time() - calc_start
+
+        # Used for (outlier-only) misclassification prediction
+        # is_misclassified = test_set[1] != pred
+        # auc_roc = metrics.roc_auc_score(is_misclassified, surp)
+        # avg_pr = metrics.average_precision_score(is_misclassified, surp)
+
+        is_outlier = np.ones(shape=(nom_surp.shape[0] + surp.shape[0]), dtype=bool)
+        is_outlier[:nom_surp.shape[0]] = 0
+        combined_surp = np.concatenate((nom_surp, surp))
+        ood_auc_roc = metrics.roc_auc_score(is_outlier, combined_surp)
+
+        result.evals[test_set_name] = TestSetEval(eval_time=calc_time,
+                                                  ood_auc_roc=ood_auc_roc,
+                                                  num_nominal_samples=nom_surp.shape[0],
+                                                  num_outlier_samples=surp.shape[0])
+
+    return result
+
+
+def save_results_to_fs(case_study: str, results: List[Result]) -> None:
+    os.makedirs(f"../results/{case_study}", exist_ok=True)
+    for res in results:
+        with open(f"results/{case_study}/{res.name}.pickle", "wb+") as f:
+            pickle.dump(res, file=f)
