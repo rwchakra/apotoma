More changes to loss calculation, gradient tape to fix

outlier_exposure_temp_folder/oe_exp.py
@@ -7,8 +7,10 @@ from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
 import tensorflow.keras.backend as K
 from scipy.special import softmax as sf
+import numpy as np
 
 def grad(model, inputs):
+    #TODO tape.gradient needs to compute on loss, not softmax. Find a way without breaking the graph
   with tf.GradientTape() as tape:
     softmax, acts = forward_activations(model, inputs, training=True)
   return softmax, tape.gradient(softmax, model.trainable_variables), acts
@@ -16,16 +18,16 @@ def grad(model, inputs):
 def forward_activations(model, x, training):
   # training=training is needed only if there are layers with different
   # behavior during training versus inference (e.g. Dropout).
-    #y_ = model(x, training=training)
+    y_ = model(x, training=training)
     outputs = [layer.output for layer in model.layers]
 
 
     pre_softmax = outputs[-2]
     get_output = K.function([model.input],
-                        [outputs[-1], pre_softmax], K.set_learning_phase(1))
-    [softmax, predictions_pre] = get_output([x])
+                        [pre_softmax], K.set_learning_phase(1))
+    [predictions_pre] = get_output([x])
 
-    return softmax, predictions_pre
+    return y_, predictions_pre
 
 def train(model, train_datagen):
     train_loss_results = []
@@ -46,40 +48,44 @@ def train(model, train_datagen):
 
     num_epochs = 10
     epoch_loss_avg = tf.keras.metrics.Mean()
-    m = tf.keras.metrics.CategoricalCrossentropy()
-    epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+    m = tf.keras.losses.CategoricalCrossentropy()
+    epoch_accuracy = tf.keras.metrics.CategoricalAccuracy()
     optimizer = tf.keras.optimizers.Adam()
 
     for epoch in range(num_epochs):
 
-
+        epoch_loss = 0
+        epoch_acc = 0
         # Training loop - using batches of 32
         for d_in, d_out in zip(train_image_generator_in, train_image_generator_out):
             data = tf.keras.layers.concatenate([d_in[0], d_out[0]], axis=0)
             target = d_in[1]
-            # Optimize the model
+            # Below, instead of calling function, maybe just write the code here
+
             activations, grads, pre_softmax = grad(model, data)
 
-            loss_value = m.update_state(activations[:len(d_in[0])], target)
-            uniform_loss = tf.reduce_mean(tf.reduce_mean(pre_softmax[len(d_in[0]):], 1) - tf.reduce_logsumexp(pre_softmax[d_in[0]:], 1))
-            loss_value += 0.5 * -uniform_loss
+            loss_value = m(activations[:len(d_in[0])], target).numpy()
+            uniform_loss = tf.reduce_mean(tf.reduce_mean(pre_softmax[len(d_in[0]):], 1) - tf.reduce_logsumexp(pre_softmax[len(d_in[0]):], 1))
+            loss_value += 0.5 * -uniform_loss.numpy()
             optimizer.apply_gradients(zip(grads, model.trainable_variables))
 
             # Track progress
-            epoch_loss_avg.update_state(loss_value)  # Add current batch loss
+            epoch_loss += loss_value
+            epoch_acc += epoch_accuracy([target], [activations[:len(d_in[0])]]).numpy()
+            #epoch_loss_avg.update_state(K.constant(loss_value))  # Add current batch loss
             # Compare predicted label to actual label
             # training=True is needed only if there are layers with different
             # behavior during training versus inference (e.g. Dropout).
-            epoch_accuracy.update_state(target, model(d_in, training=True))
+            #epoch_accuracy.update_state([target], [activations[:len(d_in[0])]])
 
         # End epoch
-        train_loss_results.append(epoch_loss_avg.result())
-        train_accuracy_results.append(epoch_accuracy.result())
+        train_loss_results.append(np.mean(epoch_loss))
+        train_accuracy_results.append(np.mean(epoch_acc))
 
-        if epoch % 50 == 0:
+        if epoch % 1 == 0:
             print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
-                                                                        epoch_loss_avg.result(),
-                                                                        epoch_accuracy.result()))
+                                                                        np.mean(epoch_loss),
+                                                                        np.mean(epoch_acc)))
 
 
 