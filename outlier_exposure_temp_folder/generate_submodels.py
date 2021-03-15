53257c6076d42c393cdf6b8f23825adb3309bc63
rwiddhi chakraborty <dejavu.chakraborty@gmail.com>
2021-03-15 18:25:24 +0100

    Changes to generate submodels for CIFAR-10 and MNIST

--- outlier_exposure_temp_folder/generate_submodels.py
+++ outlier_exposure_temp_folder/generate_submodels.py
@@ -11,26 +11,53 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D,
 from tensorflow.keras.models import load_model
 
 root = '/Users/rwiddhichakraborty/PycharmProjects/Thesis/apotoma'
-orig = load_model(root+'/model/model_mnist.h5')
+model = load_model(root+'/model/model_outexp_nosmcifar.h5')
 
-layer_list = orig.layers
-non_trainable = ['dropout', 'activation']
-for l, ltype in enumerate(layer_list):
-    layers = layer_list[0:l+1]
-    n_model = Sequential()
-    for layer in layers:
-        if 'conv2d' in layer.name:
-            n_model.add(layer)
-            n_model.add(Activation(activation='relu'))
-        else:
-            n_model.add(layer)
-    n_model.add(Flatten())
+print(model.summary())
+l_names = ['conv2d', 'conv2d_1', 'max_pooling2d', 'conv2d_2', 'conv2d_3','max_pooling2d_1',
+           'conv2d_4', 'conv2d_5', 'max_pooling2d_2','dense', 'dense_1'] #Pass in all layers except final dense softmax
 
-    for layer in n_model.layers:
-        layer.trainable = False
+for i, l_name in enumerate(l_names):
+            mid = model.get_layer(l_name).output
+            if ('conv2d' in l_name or 'dense' in l_name): #Add activation for trainable layers
+                act = Activation('relu')(mid)
+            else:
+                act = mid
+            if 'dense' not in l_name: #Flatten only if not already a dense layer
+                flat = Flatten()(act)
+            else:
+                flat = mid
+            final = Dense(10, activation='softmax')(flat)
+            n_model = tf.keras.Model(model.input, final)
+            for l in n_model.layers[:-1]:
+                l.trainable = False
 
-    n_model.add(Dense(10, activation='softmax'))
-    print(n_model.summary())
-    n_model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(), metrics='accuracy')
-    #n_model.save("submodel_{}_mnist.h5".format(l))
+            print(n_model.summary())
+            n_model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(), metrics='accuracy')
+            n_model.save(root+"/submodels_dissector/model_outexp_nosmcifar/submodel_{}.h5".format(i))
 
+# while i < len(layer_list):
+#     layers = layer_list[0:i+1]
+#     n_model = Sequential()
+#     for layer in layers:
+#
+#         if (('activation' in layer.name) or ('dropout' in layer.name)):
+#             continue
+#         elif 'conv2d' in layer.name:
+#             n_model.add(layer)
+#             n_model.add(Activation(activation='relu'))
+#         else:
+#             n_model.add(layer)
+#
+#     if ('flatten' not in layer.name) and ('dense' not in layer.name):
+#         n_model.add(Flatten())
+#
+#     for layer in n_model.layers:
+#         layer.trainable = False
+#
+#     if layer.get_config() != layer_list[-1].get_config():
+#         n_model.add(Dense(10, activation='softmax'))
+#     print(n_model.summary())
+#     n_model.compile(loss='categorical_crossentropy', optimizer=tf.keras.optimizers.Adam(), metrics='accuracy')
+#     i += 2
+#     #n_model.save("submodel_{}_mnist.h5".format(l))
