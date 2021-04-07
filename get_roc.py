8a7415f5520c5322f8348beed4ff1a9f576e039b
rwiddhi chakraborty <dejavu.chakraborty@gmail.com>
2021-04-07 13:03:06 +0200

    Recording prediction runtimes

--- get_roc.py
+++ get_roc.py
@@ -8,14 +8,17 @@
 
 import numpy as np
 from tensorflow.keras.models import load_model
-from tensorflow.keras.datasets import mnist, cifar10
+from tensorflow.keras.datasets import mnist, cifar10, fashion_mnist
 from sklearn.metrics import roc_auc_score
 from dissector_temp_folder import dissector
 import argparse
+import time
 
 root = "/Users/rwiddhichakraborty/PycharmProjects/Thesis/apotoma"
 
-x_test_corrupted = np.load('ood_data/adversarial/mnist/mnist_base_model_adv_0.03.npy')
+x_test_corrupted = np.load('mnist_corrupted.npy')
+#(_, _), (x_test_corrupted, y_test_corrupted) = fashion_mnist.load_data()
+#x_test_corrupted = x_test_corrupted.reshape(-1, 28, 28, 1)
 x_test_corrupted = (x_test_corrupted / 255.0)
 x_test_corrupted = x_test_corrupted.astype("float32")
 
@@ -26,7 +29,7 @@ x_test_nominal = x_test_nominal.astype("float32")
 
 
 model = load_model('model/model_mnist.h5')
-
+start = time.time()
 preds_corrupted = model.predict(x_test_corrupted)
 preds_nominal = model.predict(x_test_nominal)
 scores_corrupted = np.max(preds_corrupted, axis=1)
@@ -37,7 +40,8 @@ y_true_nominal = np.zeros((x_test_nominal.shape[0]))
 
 y_true = np.concatenate([y_true_corrupted, y_true_nominal])
 y_scores = np.concatenate([scores_corrupted, scores_nominal]) * -1
-
+end = time.time()
+print("Time for Softmax: ", (end - start))
 print(roc_auc_score(y_true, y_scores))
 #Vanilla sofmax MNIST-C: 0.5985
 #Vanilla softmax MNIST-adv: 1.0
@@ -46,7 +50,7 @@ args = argparse.Namespace()
 args.model_path = root+'/model/model_mnist.h5'
 args.sub_model_path = root+'/submodels_dissector_latest/model_mnist/'
 diss = dissector.Dissector(model, config=args)
-
+start = time.time()
 sv_scores_corrupted = diss.sv_score(x_test_corrupted)
 weights = diss.get_weights(growth_type='linear', alpha=0.9)
 
@@ -56,16 +60,18 @@ sv_scores_nominal = diss.sv_score(x_test_nominal)
 pv_scores_nominal = diss.pv_scores(weights, sv_scores_nominal)
 
 y_scores = np.concatenate([pv_scores_corrupted, pv_scores_nominal]) * -1
+end = time.time()
 print(roc_auc_score(y_true, y_scores))
+print("Time for dissector: ", (end - start))
 #DISSECTOR-Linear MNIST-C: 0.6144
 #DISSECTOR-Linear MNIST-Adv: 0.6218
 
 model = load_model('model/model_mnist_finetuned.h5')
 
-x_test_corrupted = np.load('ood_data/adversarial/mnist/mnist_finetuned_base_model_adv_0.03.npy')
-x_test_corrupted = (x_test_corrupted / 255.0)
-x_test_corrupted = x_test_corrupted.astype("float32")
-
+#x_test_corrupted = np.load('ood_data/adversarial/mnist/mnist_finetuned_base_model_adv.npy')
+#x_test_corrupted = (x_test_corrupted / 255.0)
+#x_test_corrupted = x_test_corrupted.astype("float32")
+start = time.time()
 preds_corrupted = model.predict(x_test_corrupted)
 preds_nominal = model.predict(x_test_nominal)
 scores_corrupted = np.max(preds_corrupted, axis=1)
@@ -76,9 +82,9 @@ y_true_nominal = np.zeros((x_test_nominal.shape[0]))
 
 y_true = np.concatenate([y_true_corrupted, y_true_nominal])
 y_scores = np.concatenate([scores_corrupted, scores_nominal]) * -1
-
+end = time.time()
 print(roc_auc_score(y_true, y_scores))
-
+print("Time for OE: ", (end - start))
 #OE MNIST-C: 0.8327
 #OE MNIST-ADV: 1.0
 
--- get_roc_cifar.py
+++ get_roc_cifar.py
@@ -8,16 +8,19 @@
 
 import numpy as np
 from tensorflow.keras.models import load_model
-from tensorflow.keras.datasets import mnist, cifar10
+from tensorflow.keras.datasets import mnist, cifar10, cifar100
 from sklearn.metrics import roc_auc_score
+from sklearn.metrics import plot_roc_curve
+import matplotlib.pyplot as plt
 from dissector_temp_folder import dissector
 import argparse
+import time
 
 root = "/Users/rwiddhichakraborty/PycharmProjects/Thesis/apotoma"
 
 
-x_test_corrupted = np.load('ood_data/adversarial/cifar10/outexp_nosmcifar_adv_0.1.npy')
-
+#x_test_corrupted = np.load('ood_data/adversarial/cifar10/outexp_nosmcifar_base_adv.npy')
+(_, _), (x_test_corrupted, y_test_corrupted) = cifar100.load_data()
 x_test_corrupted = (x_test_corrupted / 255.0)
 x_test_corrupted = x_test_corrupted.astype("float32")
 
@@ -29,11 +32,12 @@ x_test_nominal = x_test_nominal.astype("float32")
 
 model = load_model('model/model_outexp_nosmcifar.h5')
 print("Running for vanilla softmax...")
+start = time.time()
 preds_nominal = model.predict(x_test_nominal)
 scores_nominal = np.max(preds_nominal, axis=1)
 
-# for i in range(5):
-#     x_test_corrupted_intensity = x_test_corrupted[:, i]
+#for i in range(5):
+#x_test_corrupted_intensity = x_test_corrupted[:, i]
 preds_corrupted = model.predict(x_test_corrupted)
 
 scores_corrupted = np.max(preds_corrupted, axis=1)
@@ -45,19 +49,22 @@ y_true_nominal = np.zeros((x_test_nominal.shape[0]))
 y_true = np.concatenate([y_true_corrupted, y_true_nominal])
 y_scores = np.concatenate([scores_corrupted, scores_nominal]) * -1
 
+end = time.time()
 print(roc_auc_score(y_true, y_scores))
+print("Time taken for Softmax: ", (end - start))
     #Vanilla sofmax MNIST-C: 0.5985
     #Vanilla softmax MNIST-adv: 1.0
     #Vanilla sofmax CIFAR10-C: 0.63
     #Vanilla softmax CIFAR10-adv: 0.91
 
 print("Running for Dissector...")
+start = time.time()
 args = argparse.Namespace()
 args.model_path = root+'/model/model_outexp_nosmcifar.h5'
 args.sub_model_path = root+'/submodels_dissector_latest/model_outexp_nosmcifar/'
 
-# for i in range(5):
-#     x_test_corrupted_intensity = x_test_corrupted[:, i]
+#for i in range(5):
+#x_test_corrupted_intensity = x_test_corrupted[:, i]
 diss = dissector.Dissector(model, config=args)
 
 sv_scores_corrupted = diss.sv_score(x_test_corrupted)
@@ -73,21 +80,25 @@ y_true_nominal = np.zeros((x_test_nominal.shape[0]))
 y_true = np.concatenate([y_true_corrupted, y_true_nominal])
 
 y_scores = np.concatenate([pv_scores_corrupted, pv_scores_nominal]) * -1
+end = time.time()
 print(roc_auc_score(y_true, y_scores))
-# # #DISSECTOR-Linear MNIST-C: 0.6144
-# # DISSECTOR-Linear MNIST-Adv: 0.6218
-# # DISSECTOR-Linear CIFAR10-C: 0.622
-# # DISSECTOR-Linear CIFAR10-Adv: 0.8685
-#
+print("Time for Dissector: ", (end - start))
+
+# # # #DISSECTOR-Linear MNIST-C: 0.6144
+# # # DISSECTOR-Linear MNIST-Adv: 0.6218
+# # # DISSECTOR-Linear CIFAR10-C: 0.622
+# # # DISSECTOR-Linear CIFAR10-Adv: 0.8685
+# #
 model = load_model('model/model_outexp_nosmcifar_finetuned.h5')
 
-x_test_corrupted = np.load('ood_data/adversarial/cifar10/outexp_nosmcifar_finetune_adv_0.1.npy')
-x_test_corrupted = (x_test_corrupted / 255.0)
-x_test_corrupted = x_test_corrupted.astype("float32")
+#x_test_corrupted = np.load('ood_data/adversarial/cifar10/outexp_nosmcifar_finetune_adv.npy')
+#x_test_corrupted = (x_test_corrupted / 255.0)
+#x_test_corrupted = x_test_corrupted.astype("float32")
 
 print("Running for OE....")
-# for i in range(5):
-#     x_test_corrupted_intensity = x_test_corrupted[:, i]
+start = time.time()
+#for i in range(5):
+#x_test_corrupted_intensity = x_test_corrupted[:, i]
 
 preds_corrupted = model.predict(x_test_corrupted)
 preds_nominal = model.predict(x_test_nominal)
@@ -100,6 +111,9 @@ y_true_nominal = np.zeros((x_test_nominal.shape[0]))
 y_true = np.concatenate([y_true_corrupted, y_true_nominal])
 y_scores = np.concatenate([scores_corrupted, scores_nominal]) * -1
 
+end = time.time()
 print(roc_auc_score(y_true, y_scores))
+print("Time for OE: ", (end - start))
+
 
 
